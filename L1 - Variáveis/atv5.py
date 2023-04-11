ac866a468f55939d78c2be953ff34e335bcce6cb
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:26:44 -0300

    Update atv5.py

--- L1 - Variáveis/atv5.py
+++ L1 - Variáveis/atv5.py
@@ -1,8 +1,8 @@
-num = int(input("Digite um número no formato CDU: "))
+numero = int(input("Digite um número no formato CDU: "))
 
-centena = num // 100
-dezena = (num % 100) // 10
-unidade = num % 10
+centena = numero // 100
+dezena = (numero % 100) // 10
+unidade = numero % 10
 
 inverso = unidade * 100 + dezena * 10 + centena
 
