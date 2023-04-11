1a05dc281755d4dd21c491db7e29e28aa5efaaec
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:24:26 -0300

    Update atv19.py

--- L1 - Variáveis/atv19.py
+++ L1 - Variáveis/atv19.py
@@ -1,14 +1,9 @@
-#ler o número da conta
-numeroconta = int(input("Digite o número da conta corrente (3 dígitos): "))
+numerodaconta = int(input("Digite o número da conta corrente (3 dígitos): "))
 
-#inverso do número
-inverso = int(str(numeroconta)[::-1])
+inverso = int(str(numerodaconta)[::-1])
 
-#somar o número com o inverso
-soma = numeroconta + inverso
+soma = numerodaconta + inverso
 
-#calcular o dígito verificador
 digito_verificador = str(soma)[::-1][0]
 
-#resultado
 print("Dígito verificador da conta:", digito_verificador)
