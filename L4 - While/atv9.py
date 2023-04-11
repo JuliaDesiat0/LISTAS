d585cd4f636e08af1208511217950ee83c07455c
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 10:26:12 -0300

    Update atv9.py

--- L4 - While/atv9.py
+++ L4 - While/atv9.py
@@ -1,30 +1,30 @@
-votos_branco = 0
-votos_nulo = 0
-votos_kiko = 0
-votos_chaves = 0
-votos_chiquinha = 0
+votosbranco = 0
+votosnulo = 0
+votoskiko = 0
+votoschaves = 0
+votoschiquinha = 0
 
 while True:
     voto = input("Digite o número do seu voto (ou 666 para encerrar a votação): ")
     
     if voto == "1":
-        votos_branco += 1
+        votosbranco += 1
     elif voto == "2":
-        votos_nulo += 1
+        votosnulo += 1
     elif voto == "3":
-        votos_kiko += 1
+        votoskiko += 1
     elif voto == "4":
-        votos_chaves += 1
+        votoschaves += 1
     elif voto == "5":
-        votos_chiquinha += 1
+        votoschiquinha += 1
     elif voto == "666":
         break
     else:
         print("Voto inválido. Digite novamente.")
         
 print("Resultado da votação:")
-print("Branco:", votos_branco)
-print("Nulo:", votos_nulo)
-print("Kiko:", votos_kiko)
-print("Chaves:", votos_chaves)
-print("Chiquinha:", votos_chiquinha)
+print("Branco:", votosbranco)
+print("Nulo:", votosnulo)
+print("Kiko:", votoskiko)
+print("Chaves:", votoschaves)
+print("Chiquinha:", votoschiquinha)
