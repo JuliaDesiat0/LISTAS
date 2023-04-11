84a3e520e007ad9653257e8c794e08339cd5a147
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:28:26 -0300

    Update atv1.py

--- L2 - If-Else/atv1.py
+++ L2 - If-Else/atv1.py
@@ -1,8 +1,8 @@
-num = int(input("Digite um número inteiro: "))
+numero = int(input("Digite um número inteiro: "))
 
-if num < 10:
+if numero < 10:
     print("O número digitado é menor que 10.")
-elif num > 10:
+elif numero > 10:
     print("O número digitado é maior que 10.")
 else:
     print("O número digitado é igual a 10.")
