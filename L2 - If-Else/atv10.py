427180fcb1cf1720124c9e45c507c04d5ff2b3ba
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:29:01 -0300

    Update atv10.py

--- L2 - If-Else/atv10.py
+++ L2 - If-Else/atv10.py
@@ -1,10 +1,10 @@
-ano = int(input("Digite o ano: "))
+A = int(input("Digite o ano: "))
 
-if ano % 400 == 0:
+if A % 400 == 0:
     print("O ano é bissexto")
-elif ano % 100 == 0:
+elif A % 100 == 0:
     print("O ano não é bissexto")
-elif ano % 4 == 0:
+elif A % 4 == 0:
     print("O ano é bissexto")
 else:
     print("O ano não é bissexto")
