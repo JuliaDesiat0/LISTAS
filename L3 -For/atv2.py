983adacaea68d828140660c4d39e7ce40640bf28
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:58:44 -0300

    Update atv2.py

--- L3 -For/atv2.py
+++ L3 -For/atv2.py
@@ -1,12 +1,11 @@
-# leitura das dimensões da matriz
-linhas = int(input("Digite o número de linhas da matriz: "))
-colunas = int(input("Digite o número de colunas da matriz: "))
+l = int(input("Digite o número de linhas da matriz: "))
+c = int(input("Digite o número de colunas da matriz: "))
 
 # construção e impressão da matriz
-for i in range(linhas):
-  for j in range(colunas):
+for i in range(l):
+  for j in range(c):
     print(f"A{i},{j}", end="")
-    if j == colunas-1:
+    if j == c-1:
       print("#", end="")
     else:
       print(" ", end="")
