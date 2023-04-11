a2f987170bf42c4955931b71b2235f5cb1c3441c
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:30:24 -0300

    Update atv13.py

--- L2 - If-Else/atv13.py
+++ L2 - If-Else/atv13.py
@@ -1,7 +1,7 @@
 idade = int(input("Digite a idade do trabalhador: "))
-temposervico = int(input("Digite o tempo de serviço, em anos, do trabalhador: "))
+tempodeservico = int(input("Digite o tempo de serviço, em anos, do trabalhador: "))
 
-if idade >= 65 or temposervico >= 30 or (idade >= 60 and temposervico >= 25):
+if idade >= 65 or tempodeservico >= 30 or (idade >= 60 and tempodeservico >= 25):
     print("O trabalhador pode se aposentar.")
 else:
     print("O trabalhador não pode se aposentar ainda.")
