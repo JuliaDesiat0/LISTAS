1e75dcdc77299466de27fea0b3b3539abb9702a2
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:43:28 -0300

    Update atv13.py

--- L3 -For/atv13.py
+++ L3 -For/atv13.py
@@ -1,7 +1,7 @@
-num = int(input("Digite um número inteiro: "))
+numero = int(input("Digite um número inteiro: "))
 
 fatorial = 1
-for i in range(1, num+1):
+for i in range(1, numero+1):
     fatorial *= i
 
-print(f"O fatorial de {num} é {fatorial}")
+print(f"O fatorial de {numero} é {fatorial}")
