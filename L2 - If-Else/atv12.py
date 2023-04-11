6f9bc34c88283bd828456ba05d87dabff1f390f5
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:29:51 -0300

    Update atv12.py

--- L2 - If-Else/atv12.py
+++ L2 - If-Else/atv12.py
@@ -1,4 +1,4 @@
-codigoaluno = int(input("Digite o código do aluno: "))
+codigodoaluno = int(input("Digite o código do aluno: "))
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
@@ -7,7 +7,7 @@ maiornota = max(nota1, nota2, nota3)
 
 media = (4 * maiornota + 3 * (nota1 + nota2 + nota3 - maiornota)) / 6
 
-print("Código do aluno:", codigoaluno)
+print("Código do aluno:", codigodoaluno)
 print("Notas: {}, {}, {}".format(nota1, nota2, nota3))
 print("Média: {:.2f}".format(media))
 
