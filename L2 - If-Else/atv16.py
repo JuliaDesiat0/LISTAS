065e909d3f0de74ea67e4f9706c8e945e6b54ccb
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:33:19 -0300

    Update atv16.py

--- L2 - If-Else/atv16.py
+++ L2 - If-Else/atv16.py
@@ -1,17 +1,17 @@
-lados = int(input("Digite o número de lados do polígono regular: "))
-medida = float(input("Digite a medida dos lados: "))
+L = int(input("Digite o número de lados do polígono regular: "))
+M = float(input("Digite a medida dos lados: "))
 
-if lados == 3:
+if L == 3:
     print("TRIANGULO")
-    perimetro = 3 * medida
+    perimetro = 3 * M
     print("Perímetro: {:.2f}".format(perimetro))
-elif lados == 4:
+elif L == 4:
     print("QUADRADO")
-    area = medida ** 2
+    area = M ** 2
     print("Área: {:.2f}".format(area))
-elif lados == 5:
+elif L == 5:
     print("PENTAGONO")
-elif lados < 3:
+elif L < 3:
     print("Não é polígono")
 else:
     print("Polígono não identificado")
