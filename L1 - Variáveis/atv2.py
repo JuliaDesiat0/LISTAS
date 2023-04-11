ddfa901a47b4aa0ccb46e3b8c32811619cd0304e
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:25:04 -0300

    Update atv2.py

--- L1 - Variáveis/atv2.py
+++ L1 - Variáveis/atv2.py
@@ -1,3 +1,3 @@
-caractere1 = input("Digite o primeiro caractere: ")
-caractere2 = input("Digite o segundo caractere: ")
-print(f"O usuário digitou {caractere1} e {caractere2}!")
+C1 = input("Digite o primeiro caractere: ")
+C2 = input("Digite o segundo caractere: ")
+print(f"O usuário digitou {C1} e {C2}!")
