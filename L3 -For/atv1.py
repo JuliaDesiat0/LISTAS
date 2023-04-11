0e6d036b963d653b555f63f9e0ca76e12a7bc25d
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:39:03 -0300

    Update atv1.py

--- L3 -For/atv1.py
+++ L3 -For/atv1.py
@@ -1,9 +1,9 @@
-limite_superior = int(input("Digite o limite superior do intervalo desejado: "))
+limitesuperior = int(input("Digite o limite superior do intervalo desejado: "))
 incremento = int(input("Digite o incremento desejado: "))
-limite_inferior = 10
+limiteinferior = 10
 
 print("Fahrenheit \t Celsius")
-for f in range(limite_inferior, limite_superior + 1, incremento):
+for f in range(limiteinferior, limitesuperior + 1, incremento):
     c = round((f - 32) * 5 / 9, 2)
     print("{:.2f}F \t\t {:.2f}C".format(f, c))
 
