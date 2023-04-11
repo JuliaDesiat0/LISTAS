8d802c656cf1dd5296d735af6cd2414d5c44a29f
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:50:06 -0300

    Update atv17.py

--- L3 -For/atv17.py
+++ L3 -For/atv17.py
@@ -1,11 +1,11 @@
-num = int(input("Digite um número inteiro: "))
+numero = int(input("Digite um número inteiro: "))
 
-if num <= 1:
+if numero <= 1:
     print("Não é um número primo.")
 else:
     primo = True
-    for i in range(2, num):
-        if num % i == 0:
+    for i in range(2, numero):
+        if numero % i == 0:
             primo = False
             break
 
