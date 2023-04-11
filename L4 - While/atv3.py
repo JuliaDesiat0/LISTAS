7f833f770d37c534c0ad08b567fd7283bfdc0395
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:19:38 -0300

    Update atv3.py

--- L4 - While/atv3.py
+++ L4 - While/atv3.py
@@ -1,11 +1,11 @@
 soma = 0
 cont = 0
-num = 0
+numero = 0
 
 while cont < 50:
-    if num % 2 == 0:
-        soma += num
+    if numero % 2 == 0:
+        soma += numero
         cont += 1
-    num += 1
+    numero += 1
 
 print("A soma dos 50 primeiros números pares é:", soma)
