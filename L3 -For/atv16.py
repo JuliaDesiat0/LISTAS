275fe0f1f646799e353b3f2fe9f08746bcd3db12
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:48:48 -0300

    Update atv16.py

--- L3 -For/atv16.py
+++ L3 -For/atv16.py
@@ -1,14 +1,14 @@
 while True:
-    num = int(input("Digite um número inteiro positivo menor que 16 para calcular o fatorial (ou digite um número negativo para sair): "))
+    numero = int(input("Digite um número inteiro positivo menor que 16 para calcular o fatorial (ou digite um número negativo para sair): "))
 
-    if num < 0:
+    if numero < 0:
         break
-    elif num >= 16:
+    elif numero >= 16:
         print("Número muito grande. Tente novamente.")
         continue
 
     fat = 1
-    for i in range(1, num+1):
+    for i in range(1, numero+1):
         fat *= i
 
-    print(f"O fatorial de {num} é {fat}.")
+    print(f"O fatorial de {numero} é {fat}.")
