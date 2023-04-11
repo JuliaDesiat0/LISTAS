bb11b70f62452258e4c340d352b447b22a5601ab
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:17:57 -0300

    Update atv11.py

--- L4 - While/atv11.py
+++ L4 - While/atv11.py
@@ -1,7 +1,7 @@
-soma_alunos_terceira = 0
-maior_quantidade_livros = 0
-total_alunos = 0
-alunos_nao_gostam_redacao_terceira = 0
+somadosalunosterceira = 0
+maiorquantidadelivros = 0
+totalalunos = 0
+alunosnaogostamredacaoterceira = 0
 
 while True:
     serie = int(input("Digite a série do aluno (1 a 4) ou 0 para sair: "))
@@ -13,17 +13,17 @@ while True:
     redacao = int(input("Gosta de fazer redação? Digite 1 para sim ou 0 para não: "))
     
     if serie == 3:
-        soma_alunos_terceira += 1
+        somadosalunosterceira += 1
         if redacao == 0:
-            alunos_nao_gostam_redacao_terceira += 1
+            alunosnaogostamredacaoterceira += 1
         
-    if serie == 4 and livros > maior_quantidade_livros:
-        maior_quantidade_livros = livros
+    if serie == 4 and livros > maiorquantidadelivros:
+        maiorquantidadelivros = livros
         
     total_alunos += 1
 
-percentual_nao_gostam_redacao_terceira = (alunos_nao_gostam_redacao_terceira / soma_alunos_terceira) * 100 if soma_alunos_terceira != 0 else 0
+percentualnaogostamredacaoterceira = (alunosnaogostamredacaoterceira / somadosalunosterceira) * 100 if somadosalunosterceira != 0 else 0
 
-print("a) Quantidade de alunos na terceira série: ", soma_alunos_terceira)
-print("b) Maior quantidade de livros lidos por um aluno na quarta série: ", maior_quantidade_livros)
-print("c) Porcentagem de alunos que não gostam de fazer redação e estão na terceira série: ", percentual_nao_gostam_redacao_terceira, "%")
+print("a) Quantidade de alunos na terceira série: ", somadosalunosterceira)
+print("b) Maior quantidade de livros lidos por um aluno na quarta série: ", maiorquantidadelivros)
+print("c) Porcentagem de alunos que não gostam de fazer redação e estão na terceira série: ", percentualnaogostamredacaoterceira, "%")
