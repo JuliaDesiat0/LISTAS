618a261ff2023e0a462fb1bbfe699a29979484bc
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:57:28 -0300

    Update atv19.py

--- L3 -For/atv19.py
+++ L3 -For/atv19.py
@@ -1,19 +1,19 @@
-n = int(input("Digite um número inteiro: "))
+N = int(input("Digite um número inteiro: "))
 
-primo = [True] * (n+1)
+primo = [True] * (N+1)
 primo[0] = False
 primo[1] = False
 divisoes = 0
 
-for i in range(2, n+1):
+for i in range(2, N+1):
     if primo[i]:
         j = 2
-        while i*j <= n:
+        while i*j <= N:
             divisoes += 1
             primo[i*j] = False
             j += 1
 
-print("Os números primos entre 1 e", n, "são:")
+print("Os números primos entre 1 e", N, "são:")
 for i in range(2, n+1):
     if primo[i]:
         print(i)
