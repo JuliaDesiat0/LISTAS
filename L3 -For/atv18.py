a9fd44f227657a3fd3ebd1e9fcb2fb9348770075
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:56:27 -0300

    Update atv18.py

--- L3 -For/atv18.py
+++ L3 -For/atv18.py
@@ -1,12 +1,12 @@
-num = int(input("Digite um número inteiro: "))
+numero = int(input("Digite um número inteiro: "))
 
-if num <= 1:
+if numero <= 1:
     print("Não é um número primo.")
 else:
     primo = True
     divisores = []
-    for i in range(2, num):
-        if num % i == 0:
+    for i in range(2, numero):
+        if numero % i == 0:
             primo = False
             divisores.append(i)
 
