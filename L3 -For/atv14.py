ea3737e2af8a5af3a6ce2c919eb6877a26b7fbc6
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:46:59 -0300

    Update atv14.py

--- L3 -For/atv14.py
+++ L3 -For/atv14.py
@@ -1,9 +1,7 @@
 N = int(input("Digite a quantidade de números: "))
 
-# inicializa as variáveis com o primeiro número lido
 maior = menor = soma = int(input("Digite um número: "))
 
-# loop para ler os próximos números e atualizar as variáveis
 for i in range(1, N):
     num = int(input("Digite um número: "))
     if num > maior:
@@ -12,7 +10,6 @@ for i in range(1, N):
         menor = num
     soma += num
 
-# imprime os resultados
 print(f"O maior número é: {maior}")
 print(f"O menor número é: {menor}")
 print(f"A soma dos números é: {soma}")
