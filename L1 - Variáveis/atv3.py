0ae9f8c9a25644c4396faa8c32a7ce8f1ebfe2c8
JuliaDesiat0 <125393270+JuliaDesiat0@users.noreply.github.com>
2023-04-11 09:25:33 -0300

    Update atv3.py

--- L1 - Variáveis/atv3.py
+++ L1 - Variáveis/atv3.py
@@ -1,5 +1,5 @@
-num = int(input("Digite um número inteiro: "))
+numero = int(input("Digite um número inteiro: "))
 antecessor = num -1
 sucessor = num +2
-print(f"O antecessor de", num, "é", antecessor)
-print(f"O sucessor de", num, "é", sucessor)
+print(f"O antecessor de", numero, "é", antecessor)
+print(f"O sucessor de", numero, "é", sucessor)
